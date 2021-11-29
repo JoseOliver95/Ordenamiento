f60b7db1f2875a634258a58c204438bdd927107e
DELL 5470 <DELL 5470@192.168.1.104>
2021-11-28 23:10:10 -0600

    Metodo Shell agregado con un imprimir

--- src/datos/DatosDesordenados.java
+++ src/datos/DatosDesordenados.java
@@ -182,7 +182,39 @@ public class DatosDesordenados {
 	    k++;
 	  }
 	}
-    
+
+public DatosOrdenados Shellsort(int A[]){
+  int [] a = this.getCopiaValores();
+
+        int salto, aux, i;
+        boolean cambios;
+        
+  
+        for (salto = A.length / 2; salto != 0; salto /= 2) {
+            cambios = true;
+            while (cambios) {   // Mientras se intercambie algún elemento                                         
+                cambios = false;
+                for (i = salto; i < A.length; i++)   // se da una pasada
+                {
+                    if (A[i - salto] > A[i]) {       // y si están desordenados
+                        aux = A[i];                  // se reordenan
+                        A[i] = A[i - salto];
+                        A[i - salto] = aux;
+                        cambios = true;              // y se marca como cambio.                                   
+                    }
+                }
+            }
+        }
+        return new DatosOrdenados(A);
+       
+    }
+   static void Impresion (int[] numeros){
+   for (int i = 0; i < numeros.length; i++) {
+   System.out.println("["+numeros[i]+"]");
+    }
+   
+   }
+
     @Override
     public String toString(){
         String cad = "Datos DESORDENADOS: ";
